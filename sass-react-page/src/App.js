de0e14221d69d13214622db40929ceb4791cb907
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2019-03-06 16:47:34 -0600

    starting

--- sass-react-page/src/App.js
+++ sass-react-page/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      page
+          <div className="headerContainer">
+            </div>
       </div>
     );
   }
